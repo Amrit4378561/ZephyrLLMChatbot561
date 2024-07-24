da71205c2ab23904f7b90f36ea3a6b792949af9d
Amrit4378561 <Am4378561@alphacollege.me>
2024-07-24 17:49:14 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def respond(
     temperature,
     top_p,
 ):
-    system_message = "You are a good listener. You advise relaxation exercises, suggest avoiding negative thoughts, and guide through steps to manage stress. Discuss what's on your mind, or ask me for a quick relaxation exercise."
+    system_message = "AI is changing health care today and in the future, from diagnosing diseases to creating personalized treatment plans."
     messages = [{"role": "system", "content": system_message}]
 
     for val in history:
@@ -46,7 +46,7 @@ For information on how to customize the ChatInterface, peruse the gradio docs: h
 demo = gr.ChatInterface(
     respond,
     additional_inputs=[
-        gr.Textbox(value = "You are a good listener. You advise relaxation exercises, suggest avoiding negative thoughts, and guide through steps to manage stress. Discuss what's on your mind, or ask me for a quick relaxation exercise.", label="System message"),
+        gr.Textbox(value = "AI is changing health care today and in the future, from diagnosing diseases to creating personalized treatment plans.", label="System message"),
         gr.Slider(minimum=1, maximum=2048, value=512, step=1, label="Max new tokens"),
         gr.Slider(minimum=0.1, maximum=4.0, value=0.7, step=0.1, label="Temperature"),
         gr.Slider(
@@ -59,11 +59,11 @@ demo = gr.ChatInterface(
     ],
 
     examples = [ 
-        ["I feel overwhelmed with work."],
-        ["Can you guide me through a quick meditation?"],
-        ["How do I stop worrying about things I can't control?"]
+        ["How AI is used in healthcare?"],
+        ["Why AI is important in the field of healthcare?"],
+        ["What are some beenfits of using AI with healthcare?"]
     ],
-    title = 'Calm Mate 🕊️'
+    title = 'AI in Healthcare'
 )
 
 
